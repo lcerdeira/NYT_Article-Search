900ce57bddafdbe777863e79dbc5f2b59f9744fe
louise cerdeira <lcerdeira@gmail.com>
2020-06-15 21:16:24 +1000

    fix

--- js/app.js
+++ js/app.js
@@ -1,36 +1,40 @@
+function loadData() {
+    var $nyArticles = $('#ny-times-articles');
+    var articleRequest = $(".nySearch").val();
 
-function loadData(){
-  var $nyArticles = $('#ny-times-articles');
-  var articleRequest = $(".nySearch").val();
+    //Sets the articles blank everytime a new search is started.
+    $nyArticles.text("");
 
-//Sets the articles blank everytime a new search is started.
-  $nyArticles.text("");
+    //The NY API request with the value of the search input
+    //Louise Put her key here
+    //This project was based on this git repo git@github.com:Jcook894/NY-Times-Article-Search.git
 
-//The NY API request with the value of the search input.
-   var nyUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleRequest + '=&sort=newest&api-key=04af26cdd76f4b8da348f53553eadcb3';
+    var nyUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleRequest + '=&sort=newest&api-key=GJ4W64VxpQp8yz68kJcFgoi6j9GNeZGk';
 
-    $.getJSON(nyUrl, function(data){
 
-      nyArticles = data.response.docs;
-      console.log(nyArticles);
+    $.getJSON(nyUrl, function(data) {
 
-      for(var i = 0; i < nyArticles.length; i++){
-        var articles = nyArticles[i];
+        nyArticles = data.response.docs;
+        console.log(nyArticles);
 
-//Appends the the Articles in a list.
-        $nyArticles.append('<li class="article">'+
-                '<a href="'+articles.web_url+'">'+articles.headline.main+'</a>'+
-                '<p>' + articles.snippet + '</p>'+
-            '</li>');
+        for (var i = 0; i < nyArticles.length; i++) {
+            var articles = nyArticles[i];
 
-      };
-// If NY times request fails, throw an error message.
-    }).fail(function(e){
-      $nyArticles.text("NY times request could not be found.");
-      console.log("Request failed");
+
+            //Appends the the Articles in a list.
+            $nyArticles.append('<li class="article">' +
+                '<a href="' + articles.web_url + '">' + articles.headline.main + '</a>' +
+                '<p>' + articles.snippet + '</p>' +
+                '</li>');
+
+        };
+        // If NY times request fails, throw an error message.
+    }).fail(function(e) {
+        $nyArticles.text("NY times request could not be found.");
+        console.log("Request failed");
     });
 
-return false;
+    return false;
 
 };
 
